a811e04260fdafef032f87f55368221031438339
benefrihw <gjtja7@naver.com>
2024-07-11 03:22:42 +0900

    공연 생성 API

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,8 +9,6 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ShowModule } from './show/show.module';
 import { BookModule } from './book/book.module';
-import { LocalStrategy } from './auth/strategies/local.strategy';
-import { JwtStrategy } from './auth/strategies/jwt.strategy';
 
 @Module({
   imports: [
